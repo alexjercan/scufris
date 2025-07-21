ba0a3bdcf342c3b3c4f89e374b68d324a7c91e9f
Alex Jercan <jercan_alex27@yahoo.com>
2025-07-21 16:55:21 +0300

    refactor: imagegen has it's own error kind

--- internal/imagegen/simple.go
+++ internal/imagegen/simple.go
@@ -8,8 +8,6 @@ import (
 	"io"
 	"log/slog"
 	"net/http"
-
-	"github.com/alexjercan/scufris"
 )
 
 const API_GENERATE = "/generate"
@@ -33,7 +31,7 @@ func (g *Simple) Generate(ctx context.Context, request GenerateRequest) (img []b
 
 	data, err := json.Marshal(request)
 	if err != nil {
-		return img, &scufris.Error{
+		return img, &Error{
 			Code:    "IMAGEGEN_ERROR",
 			Message: "failed to marshal request",
 			Err:     fmt.Errorf("failed to marshal request: %w", err),
@@ -42,7 +40,7 @@ func (g *Simple) Generate(ctx context.Context, request GenerateRequest) (img []b
 
 	req, err := http.NewRequestWithContext(ctx, "POST", g.baseUrl+API_GENERATE, bytes.NewBuffer(data))
 	if err != nil {
-		return img, &scufris.Error{
+		return img, &Error{
 			Code:    "IMAGEGEN_ERROR",
 			Message: "failed to create image generate request",
 			Err:     fmt.Errorf("failed to create image generate request: %w", err),
@@ -52,7 +50,7 @@ func (g *Simple) Generate(ctx context.Context, request GenerateRequest) (img []b
 
 	res, err := g.httpClient.Do(req)
 	if err != nil {
-		return img, &scufris.Error{
+		return img, &Error{
 			Code:    "IMAGEGEN_ERROR",
 			Message: "failed to make image generate request",
 			Err:     fmt.Errorf("failed to make image generate request: %w", err),
@@ -61,7 +59,7 @@ func (g *Simple) Generate(ctx context.Context, request GenerateRequest) (img []b
 
 	resBody, err := io.ReadAll(res.Body)
 	if err != nil {
-		return img, &scufris.Error{
+		return img, &Error{
 			Code:    "IMAGEGEN_ERROR",
 			Message: "failed to read image generate response",
 			Err:     fmt.Errorf("failed to read image generate response: %w", err),
@@ -69,7 +67,7 @@ func (g *Simple) Generate(ctx context.Context, request GenerateRequest) (img []b
 	}
 
 	if res.StatusCode != http.StatusOK {
-		return img, &scufris.Error{
+		return img, &Error{
 			Code:    "IMAGEGEN_ERROR",
 			Message: fmt.Sprintf("Image Generate request failed with status code %d", res.StatusCode),
 			Err:     fmt.Errorf("Image Generate request failed with status code %d: %s", res.StatusCode, string(resBody)),
